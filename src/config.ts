b15ac220ac9f206a7c559c2e620d09a8259e63aa
lansky1228 <jerrywu5405@gmail.com>
2026-05-16 14:31:15 +0800

    更新 siteConfig 的副標題為「Running towards the dream.」

--- src/config.ts
+++ src/config.ts
@@ -9,7 +9,7 @@ import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
 	title: "Jerry's Blog",
-	subtitle: "A",
+	subtitle: "Running towards the dream.",
 	lang: "zh_TW", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 285, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
