0bf946a4bc2b0c3ba439551c3751fdcd0ba77696
lansky1228 <jerrywu5405@gmail.com>
2026-05-16 15:33:18 +0800

    更新 siteConfig，啟用橫幅並更改主題顏色選擇器設置

--- src/config.ts
+++ src/config.ts
@@ -13,11 +13,11 @@ export const siteConfig: SiteConfig = {
 	lang: "zh_TW", // Language code, e.g. 'en', 'zh_CN', 'ja', etc.
 	themeColor: {
 		hue: 285, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
-		fixed: false, // Hide the theme color picker for visitors
+		fixed: true, // Hide the theme color picker for visitors
 	},
 	banner: {
-		enable: false,
-		src: "assets/images/demo-banner.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
+		enable: true,
+		src: "assets/images/FZIm4hlaQAEA8u8.jpg", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
 		position: "center", // Equivalent to object-position, only supports 'top', 'center', 'bottom'. 'center' by default
 		credit: {
 			enable: false, // Display the credit text of the banner image
